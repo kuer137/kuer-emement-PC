5e341995c13b1a426d1026eaf42fd962a055ff61
kuer137 <873791976@qq.com>
2018-03-07 18:05:09 +0800

    s

---
+++ src/view/layout/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+      根目录
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'layout',
+  components: {
+    //
+  },
+  data() {
+    //
+  },
+  methods: {
+    //
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  /**/
+</style>
